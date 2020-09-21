4dbdaf4545b6993443e587f1575b15e1c791c2ff
vedransehovic <vedran.sehovic@gmail.com>
2020-09-21 15:06:01 -0400

    all CRUD done

--- app/controllers/bays_controller.rb
+++ app/controllers/bays_controller.rb
@@ -1,2 +1,61 @@
 class BaysController < ApplicationController
+    def index 
+        @bays = Bay.all 
+    end
+
+    def show 
+        set_bay
+    end
+
+    def new
+        @bay = Bay.new
+    end
+
+    def create 
+        @bay = Bay.new(bay_params)
+        if @bay.save
+            redirect_to bays_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        set_bay
+    end
+
+    def update
+        set_bay
+        if @bay.update(bay_params)
+            redirect_to bay_path
+        else
+            render :edit
+        end
+    end
+
+    def delete 
+        set_bay
+        @bay.delete
+        redirect_to bays_path
+    end
+
+    def reset 
+        reset_bay
+        redirect_to bay_path(@bay)
+    end
+
+    private
+
+    def set_bay
+        @bay = Bay.find_by_id(params[:id])
+    end
+
+    def bay_params
+        params.require(:bay).permit(:bay_number, :date, :active)
+    end
+
+    def reset_bay
+        set_bay
+        @bay.update(date: nil, active: nil)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :productions
   resources :users
   resources :bays
+  get '/bays/reset/:id', to: 'bays#reset'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
