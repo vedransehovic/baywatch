5073b47322a02154b2658e319c99a8289432db04
vedransehovic <vedran.sehovic@gmail.com>
2020-09-21 19:12:41 -0400

    incremental update 09211900

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ApplicationRecord
+    belongs_to :production
+    
     def name
         first_name + ' ' + last_name
     end
--- app/models/production.rb
+++ app/models/production.rb
@@ -1,2 +1,3 @@
-class Production < ApplicationRecord
+class Production < ApplicationRecord 
+    has_many :clients
 end
